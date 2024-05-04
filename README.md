83e144bdc5b928a3be5564dc513ee1a2cb69b6e7
Fernando <65518148+Fernando-Zzs@users.noreply.github.com>
2024-05-04 22:58:53 +0800

    repo init

---
+++ README.md
@@ -0,0 +1,14 @@
+# VAST 2022 - Mini Challenge 3 - Economics
+
+## 启动项目的步骤
+1. 确保电脑安装了node和npm，并配置了环境变量，检查一下版本
+    ```bash
+    node -v
+    npm -v
+    ```
+2. 切换目录并运行本地服务器
+    ```bash
+    cd reference_project_1 # 或者reference_project_2
+    node server.js
+    ```
+3. 浏览器访问：http://localhost:3000
---
+++ reference_project_2/server.js
@@ -0,0 +1,43 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  let filePath = '.' + req.url;
+  if (filePath === './') {
+    filePath = './index.html'; // 默认加载 index.html 文件
+  }
+
+  const extname = String(path.extname(filePath)).toLowerCase();
+  const contentType = {
+    '.html': 'text/html',
+    '.css': 'text/css',
+    '.js': 'text/javascript',
+    '.json': 'application/json',
+    '.png': 'image/png',
+    '.jpg': 'image/jpg',
+    '.gif': 'image/gif',
+  }[extname] || 'application/octet-stream';
+
+  fs.readFile(filePath, (error, content) => {
+    if (error) {
+      if (error.code === 'ENOENT') {
+        // 文件不存在
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('404 Not Found');
+      } else {
+        // 其他错误
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('500 Internal Server Error');
+      }
+    } else {
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(content, 'utf-8');
+    }
+  });
+});
+
+const port = 3000; // 指定服务器监听的端口号
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
